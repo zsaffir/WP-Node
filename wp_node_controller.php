53af979237e6138bf9161fe8c546155cf34a439e
Zeev Saffir <zsaffir@searshc.com>
2013-05-28 16:17:04 -0500

    modified node controller to use taxonomy labels

--- wp_node_controller.php
+++ wp_node_controller.php
@@ -35,19 +35,19 @@ class WP_Node_Controller {
 	public function register_post_type(){
 
 		$labels = apply_filters("wp_node_post_type_{$this->taxonomy}_labels", array(
-			'name' 					=> _x(ucfirst($this->post_type) .'s', 'post type general name'),
-			'singular_name' 		=> _x(ucfirst($this->post_type), 'post type singular name'),
-			'add_new' 				=> _x('Add New', $this->post_type),
-			'add_new_item' 			=> __('Add New ' . ucfirst($this->post_type)),
-			'edit_item' 			=> __('Edit ' . ucfirst($this->post_type)),
-			'new_item' 				=> __('New ' . ucfirst($this->post_type)),
-			'all_items' 			=> __('All ' . ucfirst($this->post_type) . 's'),
-			'view_item' 			=> __('View ' . ucfirst($this->post_type) . 's'),
-			'search_items' 			=> __('Search ' .ucfirst($this->post_type) .'s'),
+			'name' 					=> _x(ucfirst($taxonomy->name) .'s', 'post type general name'),
+			'singular_name' 		=> _x(ucfirst($taxonomy->singular_name), 'post type singular name'),
+			'add_new' 				=> _x('Add New', ucfirst($taxonomy->add_new_item)),
+			'add_new_item' 			=> __('Add New ' . ucfirst($taxonomy->add_new_item)),
+			'edit_item' 			=> __('Edit ' . ucfirst($taxonomy->edit_item)),
+			'new_item' 				=> __('New ' . ucfirst($taxonomy->new_item_name)),
+			'all_items' 			=> __('All ' . ucfirst($taxonomy->name) . 's'),
+			'view_item' 			=> __('View ' . ucfirst($taxonomy->view_item) . 's'),
+			'search_items' 			=> __('Search ' .ucfirst($taxonomy->search_items) .'s'),
 			'not_found' 			=> __("No {$this->post_type}s found"),
 			'not_found_in_trash' 	=> __("No {$this->post_type}s found in Trash"), 
 			'parent_item_colon' 	=> '',
-			'menu_name' 			=> __(ucfirst($this->post_type))
+			'menu_name' 			=> __(ucfirst($taxonomy->menu_name))
 		));
 
 
